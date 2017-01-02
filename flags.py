c9e8a9cb9a76d7b4b76c7eaf4b83231568aa4863
pypros <sobczuk.lukasz@wp.pl>
2017-01-02 19:15:34 +0100

    Added object flags and basic methods which make easy work with it

---
+++ flags.py
@@ -0,0 +1,21 @@
+class Flags:
+    z = 0
+    c = 0
+
+    def __init__(self,**kwargs):
+        self.z = kwargs['zero']
+        self.c = kwargs['carry']
+
+    def get_zero(self):
+        return self.z
+
+    def get_carry(self):
+        return self.c
+
+    def set_zero(self, *args):
+        self.z = args[0]
+
+    def set_carry(self, *args):
+        self.c = args[0]
+
+
