8c6fb99de7080a228979c89da5576b2d245d51f2
pypros <sobczuk.lukasz@wp.pl>
2017-01-02 16:09:12 +0100

    ADDCY sX, Operand  - Add Operand to Register sX with Carry

--- instruction.py
+++ instruction.py
@@ -1,3 +1,12 @@
 def add(sx, operand):
     sx = (sx + operand) % 256
     return sx
+
+
+def addcy(sx, operand, carry_in):
+    sx = (sx + operand) % 256
+    if carry_in:
+        sx = (sx + operand + 1) % 256
+    else:
+        sx = (sx + operand) % 256
+    return sx
