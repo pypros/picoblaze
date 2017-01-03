8308c9583a1c4c864ebd055825cfacaa253bd35c
Sobczuk <lukasz.sobczuk@nokia.com>
2017-01-03 14:13:59 +0100

    Cosmetic

--- general_purpose_registers.py
+++ general_purpose_registers.py
@@ -3,12 +3,12 @@ class SixteenByteWideRegisters:
         self.registers = []
         self.size = 16
         for register in range(0, self.size):
-            self.ram.append(0)
+            self.registers.append(0)
 
     def get_value(self, register_name):
-        return self.register[register_name]
+        return self.registers[register_name]
 
     def set_value(self, register, value):
-        self.register[register] = value
+        self.registers[register] = value
 
 
