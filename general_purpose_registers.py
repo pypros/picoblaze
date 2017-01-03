890f06397afe2a63bcd7809bc04b70cc61912e9c
pypros <sobczuk.lukasz@wp.pl>
2017-01-03 10:13:40 +0100

    Updated and changed container to storage data also removed method

--- general_purpose_registers.py
+++ general_purpose_registers.py
@@ -1,31 +1,14 @@
 class SixteenByteWideRegisters:
     def __init__(self):
-        self.register = {
-                "s0": 0,
-                "s1": 0,
-                "s2": 0,
-                "s3": 0,
-                "s4": 0,
-                "s5": 0,
-                "s6": 0,
-                "s7": 0,
-                "s8": 1,
-                "s9": 0,
-                "sA": 0,
-                "sB": 0,
-                "sC": 0,
-                "sD": 0,
-                "sE": 0,
-                "sF": 0
-            }
+        self.registers = []
+        self.size = 16
+        for register in range(0, self.size):
+            self.ram.append(0)
 
     def get_value(self, register_name):
         return self.register[register_name]
 
-    def set_value(self, register_name, value):
-        self.register[register_name] = value
+    def set_value(self, register, value):
+        self.register[register] = value
 
-    def rename_register(self, old_register_name, new_register_name):
-        value = self.register[old_register_name]
-        del self.register[old_register_name]
-        self.register[new_register_name] = value
+
