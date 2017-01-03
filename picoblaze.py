18935c61b25312ab0b721eb02b32f487d827395d
pypros <sobczuk.lukasz@wp.pl>
2017-01-03 19:05:14 +0100

    Added PicoBlaze Interface Signals

---
+++ picoblaze.py
@@ -0,0 +1,11 @@
+class PicoBlaze:
+    def __init__(self):
+        self.i_in_port = 0
+        self.i_interrupt = 0
+        self.i_reset = 0
+        self.i_clk = 0
+        self.o_out_port = 0
+        self.o_port_id = 0
+        self.o_read_strobe = 0
+        self.o_write_strobe = 0
+        self.o_interrupt_ack = 0
