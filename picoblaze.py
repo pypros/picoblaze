de1c4538645c5da1381079e992531291700a13ba
Sobczuk <lukasz.sobczuk@nokia.com>
2017-01-04 19:08:33 +0100

    New strategy, cpu will be monolithic object

--- picoblaze.py
+++ picoblaze.py
@@ -1,8 +1,3 @@
-from internal_element_picoblaze.instruction_program_store import Program
-from internal_element_picoblaze.general_purpose_registers import SixteenByteWideRegisters
-from internal_element_picoblaze.instruction_decoder import Decoder
-
-
 class PicoBlaze:
     def __init__(self):
         self.i_in_port = 0
@@ -14,9 +9,106 @@ class PicoBlaze:
         self.o_read_strobe = 0
         self.o_write_strobe = 0
         self.o_interrupt_ack = 0
-        self.__Program = Program()
-        self.__Decoder = Decoder()
-        self.__SixteenByteWideRegisters = SixteenByteWideRegisters()
+        self.__flag_zero = 0
+        self.__flag_carry = 0
+        self.__preserved_flag_zero = 0
+        self.__preserved_flag_carry = 0
+        self.__instruction = '0'*16
+        self.__program_counter = 0
+        self.__sixteen_byte_wide_registers = [0]*16
+        self.__operation = {
+            "01100": "ADD",
+            "01101": "ADDCY",
+            "00101": "AND",
+            "11000": "CALL",
+            "01010": "COMPARE",
+            "11110": "INTERRUPT",
+            "00011": "FETCH",
+            "00010": "INPUT",
+            "11010": "JUMP",
+            "00000": "LOAD",
+            "00110": "OR",
+            "10110": "OUTPUT",
+            "10101": "RETURN",
+            "11100": "RETURNI",
+            "10000": "SHIFT",
+            "10111": "STORE",
+            "01110": "SUB",
+            "01111": "SUBCY",
+            "01001": "TEST",
+            "00111": "XOR"
+        }
+
+# ALU
+    def __ADD(self):
+        sx = int(self.__instruction[6:10], 2)
+        if self.__instruction[5] == '1':
+            operand = int(self.__instruction[10:14], 2)
+        else:
+            operand = int(self.__instruction[10:], 2)
+
+        self.__sixteen_byte_wide_registers[sx] = (self.__sixteen_byte_wide_registers[sx]
+                                                  + self.__sixteen_byte_wide_registers[operand]) % 256
+        if (self.__sixteen_byte_wide_registers[sx]
+                + self.__sixteen_byte_wide_registers[operand]) > 255:
+            self.__flag_carry = 1
+        else:
+            self.__flag_carry = 0
+
+        if ((self.__sixteen_byte_wide_registers[sx] + self.__sixteen_byte_wide_registers[operand] == 0) or
+                (self.__sixteen_byte_wide_registers[sx] + self.__sixteen_byte_wide_registers[operand]) == 256):
+            self.__flag_zero = 1
+        else:
+            self.__flag_zero = 0
+
+    def __ADDCY(self):
+        sx = int(self.__instruction[6:10], 2)
+        if self.__instruction[5] == '1':
+            operand = int(self.__instruction[10:14], 2)
+        else:
+            operand = int(self.__instruction[10:], 2)
+
+        self.__sixteen_byte_wide_registers[sx] = (self.__sixteen_byte_wide_registers[sx]
+                                                  + self.__sixteen_byte_wide_registers[operand] + self.__flag_carry) % 256
+
+        if self.__flag_carry:
+            sx = (sx + operand + 1) % 256
+        else:
+            sx = (sx + operand) % 256
+
+        if (self.__sixteen_byte_wide_registers[sx]
+                + self.__sixteen_byte_wide_registers[operand] + self.__flag_carry) > 255:
+            self.__flag_carry = 1
+        else:
+            self.__flag_carry = 0
+
+        if ((self.__sixteen_byte_wide_registers[sx] + self.__sixteen_byte_wide_registers[operand] == 0) or
+                (self.__sixteen_byte_wide_registers[sx] + self.__sixteen_byte_wide_registers[operand] + self.__flag_carry) == 256):
+            self.__flag_zero = 1
+        else:
+            self.__flag_zero = 0
+
+    def __exec_instruction(self, name_instruction):
+        if name_instruction == "ADD":
+            self.__ADD()
+        elif name_instruction == "ADDCY":
+            self.__ADDCY()
+        else:
+            print "instruction unsupported"
 
     def run(self):
-        pass
+        # instruction = self.__program.get_instruction()
+        print self.__sixteen_byte_wide_registers
+        self.__instruction = "011011111100000000" #ADD s0,s15
+        self.__sixteen_byte_wide_registers[0] = 1
+        # self.__sixteen_byte_wide_registers[15] = 15
+        print self.__sixteen_byte_wide_registers
+        name_instruction = self.__operation[self.__instruction[0:5]]
+        print name_instruction
+        self.__exec_instruction(name_instruction)
+        print int(self.__instruction[6:10], 2)
+        print int(self.__instruction[10:14], 2)
+        print self.__sixteen_byte_wide_registers
+
+cpu = PicoBlaze()
+cpu.run()
