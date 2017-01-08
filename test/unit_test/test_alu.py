d785811e5e75f23146cee1e865e230664e73e69e
pypros <sobczuk.lukasz@wp.pl>
2017-01-08 15:11:30 +0100

    1) Fix implementation __ADD 2) Added test for flag carry and zero

--- test/unit_test/test_alu.py
+++ test/unit_test/test_alu.py
@@ -26,3 +26,29 @@ def test_ADD_sx_sy():
     cpu.run(program)
     expected_register_sx = value_register_sx + value_register_sy
     assert expected_register_sx == cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx]
+
+def test_ADD_sx_kk_flag_zero():
+    cpu = PicoBlaze()
+    #         "011000xxxxkkkkkkkk"
+    program = "011000000000000000"  # OUTPUT s0, kk
+    value_register_sx = 0
+    number_register_sx = 0
+    cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx] = value_register_sx
+    cpu.run(program)
+    expected_flag_zero = 1
+    assert expected_flag_zero == cpu._PicoBlaze__flag_zero
+
+
+def test_ADD_sx_sy_flag_carry():
+    cpu = PicoBlaze()
+    #         "011001xxxxyyyy0000"
+    program = "011001000011110000"  # OUTPUT sx, sy
+    value_register_sx = 255
+    number_register_sx = 0
+    value_register_sy = 1
+    number_register_sy = 15
+    cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx] = value_register_sx
+    cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sy] = value_register_sy
+    cpu.run(program)
+    expected_flag_carry = 1
+    assert expected_flag_carry == cpu._PicoBlaze__flag_carry
