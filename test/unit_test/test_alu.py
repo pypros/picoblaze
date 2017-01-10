d2fa6ef6380e7cb55ba87b6d20b78fb0225c72b2
pypros <sobczuk.lukasz@wp.pl>
2017-01-10 20:25:32 +0100

    Added SHIFT SLA  flag zero tests

--- test/unit_test/test_alu.py
+++ test/unit_test/test_alu.py
@@ -242,6 +242,44 @@ def test_OR_sx_sy_flag_carry():
     expected_flag_carry = 0
     assert expected_flag_carry == cpu._PicoBlaze__flag_carry
 
+
+def test_SLA():
+    cpu = PicoBlaze()
+    #         "100000xxxx00000000"
+    program = "100000000000000000"  # SLA sx
+    value_register_sx = 192
+    number_register_sx = 0
+    cpu._PicoBlaze__flag_carry = 1
+    cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx] = value_register_sx
+    cpu.run(program)
+    expected_register_sx = 129
+    assert expected_register_sx == cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx]
+
+
+def test_SLA_flag_zero():
+    cpu = PicoBlaze()
+    #         "100000xxxx00000000"
+    program = "100000000000000000"  # SLA sx
+    value_register_sx = 0
+    number_register_sx = 0
+    cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx] = value_register_sx
+    cpu.run(program)
+    expected_flag_zero = 1
+    assert expected_flag_zero == cpu._PicoBlaze__flag_zero
+
+
+def test_SLA_flag_carry():
+    cpu = PicoBlaze()
+    #         "100000xxxx00000000"
+    program = "100000000000000000"  # SLA sx
+    value_register_sx = 192
+    number_register_sx = 0
+    cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx] = value_register_sx
+    cpu.run(program)
+    expected_flag_carry = 1
+    assert expected_flag_carry == cpu._PicoBlaze__flag_carry
+
+
 def test_SLX():
     cpu = PicoBlaze()
     #         "100000xxxx00000100"
