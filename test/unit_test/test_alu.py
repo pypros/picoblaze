eaac1756312c4efdcf3ad0ff6fdabce1861c6101
pypros <sobczuk.lukasz@wp.pl>
2017-01-11 20:22:26 +0100

    Moved test STORE and FETCH from alu to instruction decoder

--- test/unit_test/test_alu.py
+++ test/unit_test/test_alu.py
@@ -163,31 +163,6 @@ def test_AND_sx_sy_flag_carry():
     assert expected_flag_carry == cpu._PicoBlaze__flag_carry
 
 
-def test_FETCH_sx_kk():
-    cpu = PicoBlaze()
-    #         "000110xxxx00kkkkkk"
-    program = "000110000000111111"  # FETCH s0, kk
-    cpu._PicoBlaze__sixty_four_byte_scratchpad_ram[63] = 127
-    number_register_sx = 0
-    cpu.run(program)
-    expected_value_register_sx = 127
-    assert expected_value_register_sx == cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx]
-
-
-def test_FETCH_sx_sy():
-    cpu = PicoBlaze()
-    #         "000111xxxxyyyy0000"
-    program = "000111000011110000"  # COMPARE sx, sy
-    number_register_sx = 0
-    value_register_sy = 63
-    number_register_sy = 15
-    cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sy] = value_register_sy
-    cpu._PicoBlaze__sixty_four_byte_scratchpad_ram[63] = 127
-    cpu.run(program)
-    expected_value_register_sx = 127
-    assert expected_value_register_sx == cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx]
-
-
 def test_COMPARE_sx_kk():
     cpu = PicoBlaze()
     #         "010100xxxxkkkkkkkk"
@@ -629,33 +604,6 @@ def test_SRX_flag_carry():
     cpu.run(program)
     expected_flag_carry = 1
     assert expected_flag_carry == cpu._PicoBlaze__flag_carry
-
-
-def test_STORE_sx_kk():
-    cpu = PicoBlaze()
-    #         "000110xxxx00kkkkkk"
-    program = "101110000000111111"  # FETCH s0, kk
-    value_register_sx = 3
-    number_register_sx = 0
-    cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx] = value_register_sx
-    cpu.run(program)
-    expected_value_record_from_sixty_four_byte_scratchpad_ram = 3
-    assert expected_value_record_from_sixty_four_byte_scratchpad_ram == cpu._PicoBlaze__sixty_four_byte_scratchpad_ram[63]
-
-
-def test_STORE_sx_sy():
-    cpu = PicoBlaze()
-    #         "000111xxxxyyyy0000"
-    program = "101111000011110000"  # FETCH s0, kk
-    value_register_sx = 3
-    number_register_sx = 0
-    value_register_sy = 63
-    number_register_sy = 15
-    cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx] = value_register_sx
-    cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sy] = value_register_sy
-    cpu.run(program)
-    expected_value_record_from_sixty_four_byte_scratchpad_ram = 3
-    assert expected_value_record_from_sixty_four_byte_scratchpad_ram == cpu._PicoBlaze__sixty_four_byte_scratchpad_ram[63]
 
 
 def test_SUB_sx_kk():
--- test/unit_test/test_instruction_decoder.py
+++ test/unit_test/test_instruction_decoder.py
@@ -1,6 +1,31 @@
 from picoblaze import PicoBlaze
 
 
+def test_FETCH_sx_kk():
+    cpu = PicoBlaze()
+    #         "000110xxxx00kkkkkk"
+    program = "000110000000111111"  # FETCH s0, kk
+    cpu._PicoBlaze__sixty_four_byte_scratchpad_ram[63] = 127
+    number_register_sx = 0
+    cpu.run(program)
+    expected_value_register_sx = 127
+    assert expected_value_register_sx == cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx]
+
+
+def test_FETCH_sx_sy():
+    cpu = PicoBlaze()
+    #         "000111xxxxyyyy0000"
+    program = "000111000011110000"  # COMPARE sx, sy
+    number_register_sx = 0
+    value_register_sy = 63
+    number_register_sy = 15
+    cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sy] = value_register_sy
+    cpu._PicoBlaze__sixty_four_byte_scratchpad_ram[63] = 127
+    cpu.run(program)
+    expected_value_register_sx = 127
+    assert expected_value_register_sx == cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx]
+
+
 def test_INPUT_sx_kk():
     cpu = PicoBlaze()
     #         "000100xxxxkkkkkkkk"
@@ -53,3 +78,30 @@ def test_OUTPUT_sx_sy():
     cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sy] = value_register_sy
     cpu.run(program)
     assert (value_register_sx, out_port_id) == (cpu.o_out_port, cpu.o_port_id)
+
+
+def test_STORE_sx_kk():
+    cpu = PicoBlaze()
+    #         "000110xxxx00kkkkkk"
+    program = "101110000000111111"  # FETCH s0, kk
+    value_register_sx = 3
+    number_register_sx = 0
+    cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx] = value_register_sx
+    cpu.run(program)
+    expected_value_record_from_sixty_four_byte_scratchpad_ram = 3
+    assert expected_value_record_from_sixty_four_byte_scratchpad_ram == cpu._PicoBlaze__sixty_four_byte_scratchpad_ram[63]
+
+
+def test_STORE_sx_sy():
+    cpu = PicoBlaze()
+    #         "000111xxxxyyyy0000"
+    program = "101111000011110000"  # FETCH s0, kk
+    value_register_sx = 3
+    number_register_sx = 0
+    value_register_sy = 63
+    number_register_sy = 15
+    cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sx] = value_register_sx
+    cpu._PicoBlaze__sixteen_byte_wide_registers[number_register_sy] = value_register_sy
+    cpu.run(program)
+    expected_value_record_from_sixty_four_byte_scratchpad_ram = 3
+    assert expected_value_record_from_sixty_four_byte_scratchpad_ram == cpu._PicoBlaze__sixty_four_byte_scratchpad_ram[63]
